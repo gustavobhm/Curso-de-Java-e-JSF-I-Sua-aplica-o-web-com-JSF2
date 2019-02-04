7085d93022da43b111fded6da69cfd5a8d9e8623
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-02-04 10:01:21 -0200

    Aula 5 - Conversão e validação de dados

--- livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -29,6 +33,10 @@ public class LivroBean {
 		return livro;
 	}
 
+	public List<Livro> getLivros() {
+		return new DAO<Livro>(Livro.class).listaTodos();
+	}
+
 	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
@@ -46,10 +54,21 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor",
+					new FacesMessage("Livro deve ter pelo menos um Autor"));
+			return;
+		} else {
+			new DAO<Livro>(Livro.class).adiciona(this.livro);
+			this.livro = new Livro();
 		}
+	}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
 	}
 
 }
